bb057fd93e4f3387af2c894a272fe56a6ff2cc51
Kybl123 <knybeldaniel@gmail.com>
2026-09-09 12:38:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -32,23 +32,16 @@ function markerIcon(p){
   return L.divIcon({className:"",html:`<div class="marker" style="background:${c[1]}">${c[2]}<span class="count">${list.length}</span></div>`,iconSize:[34,34],iconAnchor:[17,17]});
 }
 function renderMarkers(){
-  markers.forEach(m=>m.remove());
+  markers.forEach(m=>m.remove()); 
   markers.clear();
 
   places.forEach(p=>{
     const m=L.marker(
-  [p.latitude,p.longitude],
-  {
-    icon:markerIcon(p),
-    bubblingMouseEvents:false
-  }
-).addTo(map);
+      [p.latitude,p.longitude],
+      {icon:markerIcon(p)}
+    ).addTo(map);
 
-    m.on("click",e=>{
-      L.DomEvent.stopPropagation(e);
-      showPlace(p.id);
-    });
-
+    m.on("click",()=>showPlace(p.id)); 
     markers.set(p.id,m);
   });
 }
